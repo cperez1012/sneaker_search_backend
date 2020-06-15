ee8da1b0a2e7dcfafcd479d64632dd272f8bcf8a
cperez1012 <perezchristian1012@gmail.com>
2020-06-15 17:07:53 -0400

    Populated index and create routes within sneakers controller

--- app/controllers/api/v1/sneakers_controller.rb
+++ app/controllers/api/v1/sneakers_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SneakersController < ApplicationController
+
+    def index
+        sneakers = Sneaker.all
+        render json: sneakers
+      end
+    
+      def create
+        sneaker = Sneaker.new(sneaker_params)
+        if sneaker.save
+          render json: sneaker, status: :accepted
+        else
+          render json: {errors: sneaker.errors.full_messages}, status: :unprocessible_entity
+        end
+      end
+    
+      private
+    
+      def sneaker_params
+        params.require(:sneaker).permit(:name, :description, :image_url, :category_id)
+      end
+
 end
