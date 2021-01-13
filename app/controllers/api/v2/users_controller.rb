91ae01562a966d87f945c498f41bb719434268ef
cperez1012 <perezchristian1012@gmail.com>
2021-01-13 00:00:37 -0500

    updated users controller for v2, WIP adding more methods for logining in

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -2,23 +2,43 @@ class Api::V2::UsersController < ApplicationController
 
     #GET /users 
     def index
-        @users = User.all
-
-        render json: @users
+        @users = User.all_except(current_user)
     end
 
     #GET /users/id    
-    def show
-
+    def new
+        if logged_in?
+            render json: @user
+        else
+            @user = User.new
+        end
     end
 
     #POST /users
     def create
-        # @user.User.new(user_paramas)
+        @user = User.new(user_params)
+        if @user.valid?
+          @user.save
+          session[:user_id] = @user.id
+          flash[:notice] = "Signed in successfully as #{@user.username}"
+          redirect_to profile_path(@user)
+        else
+          flash[:alert] = "#{@user.errors.full_messages.to_sentence}"
+          render :new
+        end
+    end
 
-        # if @user.save
-        #     render json: @user
+       
+    def show
+        @user = User.find(params[:id])
+    end
+   
+    def profile
+        @user = User.find_by(id: session[:id])
+    end
         
-        
+   
+    def user_params 
+        params.require(:user).permit(:username, :email, :password)
     end
 end 
