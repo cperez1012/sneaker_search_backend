3b03d87159489b5c8093d3c8bfefe8c139e4c698
cperez1012 <perezchristian1012@gmail.com>
2021-01-13 00:02:37 -0500

    updated applicaiton controller with current user and logged in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    # protect_from_forgery with: :execption
+
+    helper_method :current_user, :logged_in? 
+    # :authenticate_user!
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    # def authenticate_user!
+    
+    # end
 end
