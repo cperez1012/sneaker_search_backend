16394fa863e1fbc564877efc88b5030028187ab8
cperez1012 <perezchristian1012@gmail.com>
2021-02-16 19:27:20 -0500

    switched from v2 to v1 in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    # namespace :v1 do
-    namespace :v2 do
+    namespace :v1 do
+    # namespace :v2 do
       resources :users
       resources :sneakers, only: [:index, :create, :update, :destroy, :show]
       resources :categories, only: [:index]
