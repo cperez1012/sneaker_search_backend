907414d4866a0a7bc5b90d204aaaa3fc53112188
cperez1012 <perezchristian1012@gmail.com>
2021-03-30 19:56:52 -0400

    uncommented users resources from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
     # namespace :v2 do
-      resources :users
+      # resources :users
       resources :sneakers, only: [:index, :create, :update, :destroy, :show]
       resources :categories, only: [:index]
     end
