4b56878992defc94adb3aef43c58d9825328942b
cperez1012 <perezchristian1012@gmail.com>
2021-03-30 19:51:58 -0400

    uncommented current user methods within app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::API
     # protect_from_forgery with: :execption
 
-    helper_method :current_user, :logged_in? 
-    # :authenticate_user!
+    # helper_method :current_user, :logged_in? 
+    # # :authenticate_user!
 
-    private
+    # private
 
-    def current_user
-        current_user ||= User.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-        !!current_user
-    end
-
-    # def authenticate_user!
-    
+    # def current_user
+    #     current_user ||= User.find_by(id: session[:user_id])
     # end
+
+    # def logged_in?
+    #     !!current_user
+    # end
+
+    # # def authenticate_user!
+    
+    # # end
 end
