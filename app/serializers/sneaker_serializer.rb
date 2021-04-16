a7fa82e01d8f045858c72491469aa2ee61873c81
cperez1012 <perezchristian1012@gmail.com>
2021-04-16 07:24:11 -0400

    added shoe size and quantity to sneaker serializer

--- app/serializers/sneaker_serializer.rb
+++ app/serializers/sneaker_serializer.rb
@@ -1,6 +1,6 @@
 class SneakerSerializer
   include FastJsonapi::ObjectSerializer
   
-  attributes :name, :description, :image_url, :category_id, :category
+  attributes :name, :description, :image_url, :quantity, :shoe_size, :category_id, :category
   set_key_transform :camel_lower
 end
